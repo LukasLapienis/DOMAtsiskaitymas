6349f9549d52fc3435d6ca0c24e75a1292ecb8c7
LukasLapienis <lapienislukasp@gmail.com>
2023-12-29 00:55:49 +0200

    update

--- 03/script.js
+++ 03/script.js
@@ -5,6 +5,7 @@ const toDoItemsArray = localStorage.getItem("toDoItems") ? JSON.parse(localStora
 
 function newItemAdd(inputItem, priority) {
   const newItem = {
+    isDone: "No",
     value: inputItem.value,
     priority: priority.value
   }
@@ -15,20 +16,49 @@ function newItemAdd(inputItem, priority) {
 const list = document.querySelector(".toDoList")
 const listTable = document.createElement("table")
 listTable.style.border = "1px solid black"
+listTable.style.borderCollapse = "collapse"
 
 function displayList() {
   listTable.innerHTML = ""
 
   for (let i = 0; i < toDoItemsArray.length; i++) {
     const listItem = document.createElement("tr")
+
+    const listItemCheckbox = document.createElement("td");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = toDoItemsArray[i].isDone === "Yes";
+    listItemCheckbox.appendChild(checkbox);
+
     const listItemText = document.createElement("td")
     listItemText.textContent = toDoItemsArray[i].value
+    listItemText.style.width = "300px"
+    if (toDoItemsArray[i].isDone === "Yes") {
+      listItemText.style.textDecoration = "line-through"
+    }
+
     const listItemPriority = document.createElement("td")
     listItemPriority.textContent = toDoItemsArray[i].priority
+    listItemPriority.style.textAlign = "center"
+    listItemPriority.style.width = "80px"
+
     const listItemEdit = document.createElement("td")
     listItemEdit.setAttribute("class", "fa-solid fa-pen-to-square")
+
     const listItemDelete = document.createElement("td")
     listItemDelete.setAttribute("class", "fa-solid fa-trash")
+
+    checkbox.addEventListener("change", () => {
+      if (checkbox.checked) {
+        listItemText.style.textDecoration = "line-through";
+        toDoItemsArray[i].isDone = "Yes";
+        localStorage.setItem("toDoItems", JSON.stringify(toDoItemsArray));
+      } else {
+        listItemText.style.textDecoration = "none";
+        toDoItemsArray[i].isDone = "No";
+        localStorage.setItem("toDoItems", JSON.stringify(toDoItemsArray));
+      }
+    });
 
     listItemEdit.addEventListener("click", () => {
       const text = listItemText.textContent;
@@ -37,14 +67,25 @@ function displayList() {
       const inputField = document.createElement("input");
       inputField.type = "text";
       inputField.value = text;
+      inputField.style.width = "300px"
 
+      inputField.addEventListener("keypress", e => {
+        if (e.key === "Enter") {
+          updateTask()
+        }
+      });
       inputField.addEventListener("blur", () => {
+        updateTask()
+      });
+
+      function updateTask() {
         toDoItemsArray[i].value = inputField.value;
         localStorage.setItem("toDoItems", JSON.stringify(toDoItemsArray));
         displayList();
-      });
-
+      }
+    
       listItemText.append(inputField)
+      inputField.focus()
     })
 
     listItemDelete.addEventListener("click", () => {
@@ -53,7 +94,7 @@ function displayList() {
       localStorage.setItem("toDoItems", JSON.stringify(toDoItemsArray))
     })
 
-    listItem.append(listItemText, listItemPriority, listItemEdit, listItemDelete)
+    listItem.append(listItemCheckbox, listItemText, listItemPriority, listItemEdit, listItemDelete)
     listTable.append(listItem)
     list.append(listTable)
   }
@@ -71,6 +112,5 @@ document.querySelector(".toDoInputAdd").addEventListener("click", e => {
   newItemAdd(inputItem, priority)
   displayList()
 })
-
 
 displayList()
